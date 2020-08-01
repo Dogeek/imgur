0f0df67f2a72f994b049c906ddc6e45e81fe689c
Dogeek <Dogeek@users.noreply.github.com>
2020-08-01 14:25:35 +0200

    [image] image model and controller

---
+++ imgur/controllers/image.py
@@ -0,0 +1,64 @@
+from imgur.models.image import Image
+from imgur.utils import detect_type
+from imgur.exceptions import ImgurClientError
+from imgur.controllers.base import Controller
+
+
+class ImageController(Controller):
+    def image(self, image_hash):
+        data = self.get(f'/3/image/{image_hash}').json()['data']
+        return Image(self.session, baseurl=self.baseurl, data=data)
+
+    def upload(self, image=None, video=None, album=None, type=None,
+                     name=None, title=None, description=None,
+                     disable_audio=True):
+        if (image is None and video is None) or (image is not None and video is not None):
+            raise ImgurClientError('You need to provide either an image or a video to upload')
+
+        accepted_types = (
+            'video/mp4', 'video/webm', 'video/x-matroska', 'video/quicktime',
+            'video/x-flv', 'video/x-msvideo', 'video/x-ms-wmv', 'video/mpeg',
+        )
+
+        if video is not None and mimetypes.guess_type(video)[0] not in accepted_types:
+            raise ImgurClientError('Video mimetype is not accepted. Accepted types: {", ".join(t.split("/")[1] for t in accepted_types)}')
+
+        payload = {}
+        if image is not None:
+            payload.update(image=image)
+        if video is not None:
+            payload.update(video=video)
+        if album is not None:
+            payload.update(album=album)
+        if type is None:
+            payload.update(type=detect_type(image or video))
+        else:
+            payload.update(type=type)
+        if name is None:
+            payload.update(name=name)
+        if title is None:
+            payload.update(title=title)
+        if description is None:
+            payload.update(description=description)
+        if video is not None:
+            payload.update(disable_audio=int(disable_audio))
+
+        data = self.post('/3/upload', data=payload).json()
+        image = Image(self.session)
+        image.set(**data['data'])
+        return image
+
+    def delete(self, hash):
+        return super().delete(f'/3/image/{hash}').json()['data']
+
+    def update(self, hash, title=None, description=None):
+        payload = {}
+        if title is None:
+            payload.update(title=title)
+        if description is None:
+            payload.update(description=description)
+
+        return self.post(f'/3/image/{hash}', data=payload).json()['data']
+
+    def favorite(self, hash):
+        return self.post(f'/3/image/{hash}/favorite').json()['data']
---
+++ imgur/models/image.py
@@ -0,0 +1,45 @@
+import mimetypes
+
+from imgur.models.base import Model
+
+
+class Image(Model):
+    def init(self):
+        self.id = ''
+        self.title = ''
+        self.description = ''
+        self.datetime = 0
+        self.type = ''
+        self.animated = False
+        self.width = 0
+        self.height = 0
+        self.size = 0
+        self.views = 0
+        self.bandwidth = 0
+        self.deletehash = ''
+        self.name = ''
+        self.section = ''
+        self.link = ''
+        self.gifv = ''
+        self.mp4 = ''
+        self.mp4_size = 0
+        self.looping = False
+        self.favorite = False
+        self.nsfw = None
+        self.vote = ''
+        self.in_gallery = False
+
+    def delete(self):
+        return super().delete(f'/3/image/{self.deletehash}').json()['data']
+
+    def update(self, title=None, description=None):
+        payload = {}
+        if title is None:
+            payload.update(title=title)
+        if description is None:
+            payload.update(description=description)
+
+        return self.post(f'/3/image/{self.deletehash}', data=payload).json()['data']
+
+    def favorite(self):
+        return self.post(f'/3/image/{self.deletehash}/favorite').json()['data']
