1f233abe95d0ae27572bc8e4f954ef188ba4f13e
Dogeek <Dogeek@users.noreply.github.com>
2020-08-03 01:26:59 +0200

    [docs,utils] Docs for utils and client ref in ImgurSession

--- imgur/utils.py
+++ imgur/utils.py
@@ -11,6 +11,13 @@ from imgur.exceptions import ImgurRateLimitExceeded, ImgurNotAuthenticated
 
 
 def detect_type(img_or_vid):
+    '''
+    Detects whether an URI is a base64 encoded string, an URL or a binary file.
+
+    :param img_or_vid: The image or video to detect the UploadType of
+    :returns: UploadType.BASE54, UploadType.URL or UploadType.BINARY
+    :rtype: imgur.enums.UploadType
+    '''
     if re.search(r'^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$', img_or_vid):
         return UploadType.BASE64
     if re.search(r'^https?://', img_or_vid):
@@ -19,6 +26,14 @@ def detect_type(img_or_vid):
 
 
 def get_version(version):
+    '''
+    Parses a version tuple into a version string
+
+    :param version: version tuple or string
+    :type version: Union(tuple, str)
+    :returns: version string
+    :rtype: str
+    '''
     if isinstance(version, str):
         return version
     return '.'.join(str(v) for v in version)
@@ -27,13 +42,15 @@ class ImgurSession(Session):
     '''
     A session to make requests to imgur. Inherits from requests.Session.
     '''
-    def __init__(self, baseurl, *args, **kwargs):
+    def __init__(self, client, *args, **kwargs):
         '''
         :params baseurl: the base url to use for API calls. It should be
                          'https://api.imgur.com/' unless the app is commercial.
         '''
         super().__init__(*args, **kwargs)
-        self.baseurl = baseurl
+        self.baseurl = client.baseurl
+        self.client = client
+
         self.last_response_headers = {}
         self.etags = {}
         self.response_cache = {}
@@ -80,6 +97,10 @@ class ImgurSession(Session):
 
     @property
     def rate_limit_reached(self):
+        '''
+        Property that checks the last response headers for rate limting info,
+        and returns whether or not the rate limit has been reached
+        '''
         reached_user = self.last_response_headers['X-RateLimit-UserRemaining'] <= 0
         reached_app = self.last_response_headers['X-RateLimit-ClientRemaining'] <= 0
         has_reset = self.last_response_headers['X-RateLimit-UserReset'] >= time.time()
@@ -90,10 +111,23 @@ class ImgurSession(Session):
 
     @property
     def is_authed(self):
+        '''
+        Property that checks the Authorization request header.
+
+        Imgur supports 2 authorization methods
+            - Client-Id : unauthenticated, only using the app's client id
+            - Bearer : a bearer OAuth2 token, authenticating the app to the user.
+
+        On the second case, this property returns True, and False otherwise
+        '''
         return 'Bearer' in self.session.headers['Authorization']
 
 
 def auth_required(func):
+    '''
+    A decorator for methods that only support calls
+    from authenticated apps.
+    '''
     def decorated(self, *args, **kwargs):
         if self.session.is_authed:
             return func(self, *args, **kwargs)
