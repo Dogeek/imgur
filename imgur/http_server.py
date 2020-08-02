510c5b30f03aaeb544b70165c0086da2e433c08c
Dogeek <Dogeek@users.noreply.github.com>
2020-08-03 01:27:42 +0200

    [docs] Docstrings on http_server

--- imgur/http_server.py
+++ imgur/http_server.py
@@ -2,7 +2,11 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
 
-class HandlerInterrupt(Exception):
+class HandlerInterrupt(BaseException):
+    '''
+    Interrupt for the HTTP Server. Holds the data for the access token, refresh token
+    and for the expires_at query parameters imgurs sends to the callback.
+    '''
     def __init__(self, server, **kwargs):
         for k, v in kwargs.items():
             setattr(self, k, v[0])
@@ -10,5 +14,10 @@ class HandlerInterrupt(Exception):
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    '''
+    Custom request handler based on http.server.BaseHTTPRequestHandler
+
+    raises an HandlerInterrupt when a GET request is served to the server.
+    '''
     def do_GET(self):
         raise HandlerInterrupt(self.server, **parse_qs(self.path[2:]))
