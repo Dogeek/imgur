b686a82b8d158b9d7a4341e4566d044bcc6b0da4
Dogeek <Dogeek@users.noreply.github.com>
2020-08-03 21:13:52 +0200

    [controllers/base] Make callable which aliases to controller name

--- imgur/controllers/base.py
+++ imgur/controllers/base.py
@@ -1,2 +1,10 @@
 class Controller:
     """Base class for imgur controllers"""
+    def __init__(self, session):
+        self.session = session
+
+    def __call__(self, *args, **kwargs):
+        return getattr(
+            self,
+            self.__class__.__name__.split('Controller')[0].lower()
+        )(*args, **kwargs)
